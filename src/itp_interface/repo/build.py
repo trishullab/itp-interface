03c73b9cbe6dc936891dc6d22c80e0022a15d4de
Amitayush Thakur <amitayush@utexas.edu>
2024-05-05 21:33:23 +0000

    Fixed the build script.

--- src/itp_interface/repo/build.py
+++ src/itp_interface/repo/build.py
@@ -29,33 +29,50 @@ class RepoBuilder:
         with open(yaml_file, 'r') as stream:
             data = yaml.safe_load(stream)
             repos = data.get('repos', [])
-            # Using a multiprocessing Pool for parallel building
-            with Pool(processes=len(repos)) as pool:
-                pool.map(self._clone_and_build_repo, repos)
-
-    def _clone_and_build_repo(self, repo):
+        # Using a multiprocessing Pool for parallel building
+        with Pool(processes=len(repos)) as pool:
+            pool.map(self.clone_repo, repos)
+        
+        # Build cannot be parallelized because of opam locks
+        for repo in repos:
+            self.build_repo(repo)
+    
+    def clone_repo(self, repo):
+        repo_name = next(iter(repo))
+        repo_data = repo[repo_name]
+        self.cloner.clone_repo(repo_data)
+    
+    def build_repo(self, repo):
+        print(f"Building repo: {repo}")
         repo_name = next(iter(repo))
         repo_data = repo[repo_name]
         build_cmds = repo_data.get('build_cmds', [])
         build_path = os.path.join(self.folder, repo_name)
-        self.cloner.clone_repo(repo_data)
         self._build_repo(build_path, build_cmds, self.disable_build)
+        print(f"Finished building repo: {repo}")
 
     def _build_repo(self, build_path, build_cmds, build_disabled):
         random_id = random.randint(0, 100000)
         os.chdir(build_path)
         # Make a temporary shell script to run all build commands
         with open(f"temptodelbuild{random_id}.sh", 'w') as f:
+            f.write("#!/bin/bash\n")
             for cmd in build_cmds:
                 f.write(f"{cmd}\n")
-        os.system(f"chmod +x temptodelbuild{random_id}.sh")
+        os.system(f"chmod 777 ./temptodelbuild{random_id}.sh")
         if not build_disabled:
             # Run the shell script
             os.system(f"./temptodelbuild{random_id}.sh")
             # Clean up the shell script
-            os.remove(f"temptodelbuild{random_id}.sh")
+            os.remove(f"./temptodelbuild{random_id}.sh")
 
 # Example usage
 if __name__ == "__main__":
-    builder = RepoBuilder(".repo", disable_build=True)
-    builder.build_repos("/home/amthakur/Project/itp-interface/src/itp_interface/main/config/repo/coq_repos.yaml")
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--repo_yaml", help="Path to the yaml file containing repo data")
+    parser.add_argument("--disable_build", action="store_true", help="Disable building the repos")
+    parser.add_argument("--repo_folder", default=".repo", help="Folder to clone and build the repos in")
+    args = parser.parse_args()
+    builder = RepoBuilder(args.repo_folder, args.disable_build)
+    builder.build_repos(args.repo_yaml)
--- src/itp_interface/scripts/setup.sh
+++ src/itp_interface/scripts/setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [[ ! -d "src/itp_interface/scripts" ]]; then
     # Raise an error if the scripts directory is not present
     echo "Please run this script from the root of the repository, cannot find src/scripts"
