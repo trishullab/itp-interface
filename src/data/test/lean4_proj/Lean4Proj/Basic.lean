ef49f10f07b2d9324bf1adbf27d13e24a4f3ab76
Amitayush Thakur <amthakur@cs.utexas.edu>
2025-02-27 16:59:43 -0600

    Added `calc` proof test

--- src/data/test/lean4_proj/Lean4Proj/Basic.lean
+++ src/data/test/lean4_proj/Lean4Proj/Basic.lean
@@ -4,14 +4,29 @@ def hello := "world"
 
 theorem test (p q : Prop) (hp : p) (hq : q)
 : p ∧ q ∧ p := by
-    apply And.intro
-    exact hp
-    apply And.intro
-    exact hq
-    exact hp
+apply And.intro
+exact hp
+apply And.intro
+exact hq
+exact hp
 
 
 theorem test2 : p -> q -> p ∧ q ∧ p := fun hp hq => ⟨hp, ⟨hq, hp⟩⟩
+
+
+-- show a proof which uses calc
+theorem test_calc (n: Nat) : n^2 + 2*n + 1 = (n + 1)*(n + 1) := by
+calc
+    _ = n^2 + n*2 + 1 := by rw [Nat.mul_comm 2 n]
+    _ = n^2 + (n + n) + 1 := by rw [Nat.mul_two]
+    _ = n^2 + n + n + 1 := by rw [←Nat.add_assoc]
+    _ = n*n + n + n + 1 := by rw [Nat.pow_two]
+    _ = n*n + n*1 + n + 1 := by rw [Nat.mul_one n]
+    _ = n*(n + 1) + n + 1 := by rw [Nat.left_distrib n n 1]
+    _ = n*(n + 1) + (n + 1) := by rw [Nat.add_assoc]
+    _ = n*(n + 1) + 1*(n + 1) := by rw (config := { occs := .pos [2]}) [←Nat.mul_one (n + 1), Nat.mul_comm]
+    _ = (n + 1)*(n + 1) := by rw [Nat.right_distrib n 1 (n + 1)]
+done
 
 end Lean4Proj1
 
@@ -21,11 +36,12 @@ example : p -> q -> p ∧ q ∧ p := fun hp hq => ⟨hp, ⟨hq, hp⟩⟩
 
 theorem test (p q : Prop) (hp : p) (hq : q)
 : p ∧ q ∧ p := by
-    apply And.intro
-    exact hp
-    apply And.intro
-    exact hq
-    exact hp
+apply And.intro
+exact hp
+apply And.intro
+exact hq
+exact hp
+done
 
 theorem test3 (p q : Prop) (hp : p) (hq : q)
 : p ∧ q ∧ p := by
